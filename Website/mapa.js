b4011abbd7ba025bceeb49ed05fcbef22b92d516
Skyr <skirlaxxx@gmail.com>
2022-06-20 15:48:47 +0200

    Updates

---
+++ Website/mapa.js
@@ -0,0 +1,12 @@
+var center = SMap.Coords.fromWGS84(14.4179, 50.12655);
+var m = new SMap(JAK.gel("m"), center, 13);
+m.addDefaultLayer(SMap.DEF_BASE).enable();
+m.addDefaultControls();
+
+var layer = new SMap.Layer.Marker();
+m.addLayer(layer);
+layer.enable();
+
+var options = {};
+var marker = new SMap.Marker(center, "myMarker", options);
+layer.addMarker(marker);
