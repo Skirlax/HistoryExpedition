9a938e3abdacd6dc94a831fb78a6941cb274aa4b
Skirlax <55953556+Skirlax@users.noreply.github.com>
2022-06-20 17:32:34 +0200

    Co-authored-by: Bonsaie <Bonsaie@users.noreply.github.com>

--- Website/mapa.js
+++ Website/mapa.js
@@ -1,4 +1,5 @@
-var center = SMap.Coords.fromWGS84(14.4179, 50.12655);
+var center = SMap.Coords.fromWGS84(13.8643006, 49.6019553);
+var ryba = SMap.Coords.fromWGS84(13.8433614, 49.5772531);
 var m = new SMap(JAK.gel("m"), center, 13);
 m.addDefaultLayer(SMap.DEF_BASE).enable();
 m.addDefaultControls();
@@ -7,6 +8,37 @@ var layer = new SMap.Layer.Marker();
 m.addLayer(layer);
 layer.enable();
 
-var options = {};
-var marker = new SMap.Marker(center, "myMarker", options);
+options = {
+title: "Podcast",
+};
+var znacka = JAK.mel("div");
+var obrazek = JAK.mel(
+"img",
+{
+    src: "https://upload.wikimedia.org/wikipedia/commons/6/6e/JJryba.jpg",
+},
+{ width: 54, height: 73 }
+);
+znacka.appendChild(obrazek);
+// var popisek = JAK.mel(
+//   "div",
+//   {},
+//   {
+//     position: "absolute",
+//     left: "0px",
+//     top: "2px",
+//     textAlign: "center",
+//     width: "22px",
+//     color: "white",
+//     fontWeight: "bold",
+//   }
+// );
+// popisek.innerHTML = "JJR";
+// znacka.appendChild(popisek);
+var card = new SMap.Card();
+card.getHeader().innerHTML = "<strong>Jakub Jan Ryba</strong>";
+card.getBody().innerHTML = "About Jakub Jan Ryba";
+var marker = new SMap.Marker(ryba, "myMarker", { url: znacka });
+marker.decorate(SMap.Marker.Feature.Card, card);
+
 layer.addMarker(marker);
