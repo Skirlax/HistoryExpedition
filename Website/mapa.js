77b8eb0cc3c3d5dd5c3e7633d700d42e140e9cc6
Skyr <skirlaxxx@gmail.com>
2022-06-22 14:02:54 +0200

    update

--- Website/mapa.js
+++ Website/mapa.js
@@ -1,3 +1,24 @@
+// let podcast_props = {
+//   tea1: [
+//     [45, 30],
+//     [13.8607011, 49.6057875],
+//     "https://www.staryrozmital.cz/news/a12-dil-vila-manzelu-vorzovych/",
+//     "jmeno",
+//     "Aloisie Vorzová se společně s manželem Josefem přestěhovali do Rožmitálu pod Třemšínem krátce po zabrání Sudet. Během druhé světové války se jejich dům stal místem, kde se scházeli místní odbojáři. V roce 1943 se ve vile objevil jako nájemník jistý Miroslav Náhlovský. Tehdy ještě nikdo nemohl tušit, jak tento mladý student zkomplikuje životy mnoha obyvatel Rožmitálu. Příběh vily je také příběhem odvahy a zrady za časů protektorátu.",
+//   ],
+//   tea2: [
+//     [51, 34],
+//     [13.8622922, 49.6021342],
+//     "https://upload.wikimedia.org/wikipedia/commons/2/28/Deska_odboj_zamekrozmital.jpg",
+//     "jmeno",
+//     "Odboj v Rožmitálu pod Třemšínem začal téměř okamžitě po zabrání Sudet. Z počátku se jednalo spíše o ojedinělé akce bez větších dopadů a s malou mírou organizovanosti. Změnu přinesl František Lízl, kterému se podařilo navázat spolupráci se skupinou odbojářů z Voltuše, kterou vedli Jaroslav Pompl a Alois Hovorka. Obě skupiny se roku 1942 spojily, a velitelem se stal právě František Lízl. Klíčovým úkolem této skupiny byla distribuce ilegálních tiskovin. Později, když se v Brdech začali objevovat sovětští vojáci, kteří uprchli z nacistického zajetí, se tato skupina starala o jejich přístřeší a stravu. V roce 1943 došlo k rozsáhlé razii, při které klatovské gestapo pozatýkalo téměř všechny představitele protinacistického odboje v Brdech. Někteří odbojáři se následně snažili ukrýt v lesích nebo na tajných adresách, jiní se při dopadení bránili se zbraní v ruce. Mnoho z nich však padlo nebo bylo umučeno či popraveno.",
+//   ],
+// };
+
+// for (let i in podcast_props) {
+//   console.log(podcast_props[i][2]);
+// }
+
 var center = SMap.Coords.fromWGS84(13.8643006, 49.6019553);
 var ryba = SMap.Coords.fromWGS84(13.8433614, 49.5772531);
 var m = new SMap(JAK.gel("m"), center, 13);
@@ -9,18 +30,72 @@ m.addLayer(layer);
 layer.enable();
 
 options = {
-title: "Podcast",
+  title: "Podcast",
 };
-var znacka = JAK.mel("div");
-var obrazek = JAK.mel(
-"img",
-{
-    src: "https://upload.wikimedia.org/wikipedia/commons/6/6e/JJryba.jpg",
-},
-{ width: 54, height: 73 }
-);
-znacka.appendChild(obrazek);
-// var popisek = JAK.mel(
+
+var podcast_props = {
+  tea1: [
+    [45, 30],
+    [13.8607011, 49.6057875],
+    "https://051cf0dd00.clvaw-cdnwnd.com/047b3ee423497877efe093014898febe/200000759-b0f5cb1738/Vorzovi.JPG",
+    "Vila číslo 112 v Rybově ulici",
+    "Aloisie Vorzová se společně s manželem Josefem přestěhovali do Rožmitálu pod Třemšínem krátce po zabrání Sudet. Během druhé světové války se jejich dům stal místem, kde se scházeli místní odbojáři. V roce 1943 se ve vile objevil jako nájemník jistý Miroslav Náhlovský. Tehdy ještě nikdo nemohl tušit, jak tento mladý student zkomplikuje životy mnoha obyvatel Rožmitálu. Příběh vily je také příběhem odvahy a zrady za časů protektorátu.",
+  ],
+  tea2: [
+    [51, 34],
+    [13.8622922, 49.6021342],
+    "https://upload.wikimedia.org/wikipedia/commons/2/28/Deska_odboj_zamekrozmital.jpg",
+    "Odboj na Rožmitálsku",
+    "Odboj v Rožmitálu pod Třemšínem začal téměř okamžitě po zabrání Sudet. Z počátku se jednalo spíše o ojedinělé akce bez větších dopadů a s malou mírou organizovanosti. Změnu přinesl František Lízl, kterému se podařilo navázat spolupráci se skupinou odbojářů z Voltuše, kterou vedli Jaroslav Pompl a Alois Hovorka. Obě skupiny se roku 1942 spojily, a velitelem se stal právě František Lízl. Klíčovým úkolem této skupiny byla distribuce ilegálních tiskovin. Později, když se v Brdech začali objevovat sovětští vojáci, kteří uprchli z nacistického zajetí, se tato skupina starala o jejich přístřeší a stravu. V roce 1943 došlo k rozsáhlé razii, při které klatovské gestapo pozatýkalo téměř všechny představitele protinacistického odboje v Brdech. Někteří odbojáři se následně snažili ukrýt v lesích nebo na tajných adresách, jiní se při dopadení bránili se zbraní v ruce. Mnoho z nich však padlo nebo bylo umučeno či popraveno.",
+  ],
+  //   taischa: [
+  //     []
+  //   //
+  oscar: [
+    [37, 51],
+    [13.8641825, 49.6018411],
+    "../289006535_1246095682799872_5892602752608219913_n.jpg",
+    "Sametová revoluce a pálení totality v Rožmitále pod Třemšínem",
+    'Dny změn v regionu poodhalují svědectví pamětníků a fotografie ukryté v místním muzeu. První významná demonstrace se odehrála na zdejším náměstí již 27. 11. 1989 u příležitosti generální stávky, nad hlavami desítek občanů vlály státní vlajky, lidé provolávali hesla podporující stoupence demokratických změn. Ke shromážděným promluvili z balkonu radnice, který byl ozdoben nápisem "Chceme dialog, nechceme rozvracet!"zástupci právě se formujícího místního Občanského fóra. A k otevřeným diskuzím zde skutečně došlo. V následujících dnech proběhly v místním kině veřejné diskuse, kterých se zúčastnili studenti pražských vysokých škol, herec Otakar Brousek st., moderátor Československé televize Jan Vávra i představitelé tehdejšího komunistického vedení města. Do debat se zapojili také zdejší občané p. Hásek, p. Králíček a p. Mařík, kteří byli ještě nedávno za své názory pronásledování a vězněni. Poslední tečku za totalitou pojali místní symbolicky. Při tradičním pálení čarodějnic (30. 4. 1990), tedy den před svátkem práce dosud zneužívaným komunistickou propagandou, spálili místní občané na velké hranici za městem staré transparenty s hesly, které dříve propagovaly komunistickou ideologii. Na jejím vrcholu byla umístěna hlava čarodějnice a velký nápis "Soudružka totalita už dočarovala!"',
+  ],
+};
+
+var obrazky = [];
+
+for (var i in podcast_props) {
+  podcast_props[i][3] = podcast_props[i][3].toUpperCase() + ":";
+  var obrazek = JAK.mel(
+    "img",
+    {
+      src: podcast_props[i][2],
+    },
+    { width: podcast_props[i][0][0], height: podcast_props[i][0][1] }
+  );
+
+  var znacka = JAK.mel("div");
+
+  znacka.appendChild(obrazek);
+  var card = new SMap.Card();
+
+  card.setSize(600, 250);
+  card.getHeader().innerHTML = podcast_props[i][3];
+  card.getBody().innerHTML = podcast_props[i][4];
+  // add a space between the body and the header
+  card.getHeader().appendChild(document.createElement("p"));
+
+  var marker = new SMap.Marker(
+    SMap.Coords.fromWGS84(podcast_props[i][1][0], podcast_props[i][1][1]),
+    podcast_props[i][3],
+    { url: znacka }
+  );
+  marker.decorate(SMap.Marker.Feature.Card, card);
+
+  layer.addMarker(marker);
+}
+// for (let obrazek in obrazky) {
+//   znacka.appendChild(obrazek);
+// }
+// let popisek = JAK.mel(
 //   "div",
 //   {},
 //   {
@@ -35,10 +110,3 @@ znacka.appendChild(obrazek);
 // );
 // popisek.innerHTML = "JJR";
 // znacka.appendChild(popisek);
-var card = new SMap.Card();
-card.getHeader().innerHTML = "<strong>Jakub Jan Ryba</strong>";
-card.getBody().innerHTML = "About Jakub Jan Ryba";
-var marker = new SMap.Marker(ryba, "myMarker", { url: znacka });
-marker.decorate(SMap.Marker.Feature.Card, card);
-
-layer.addMarker(marker);
